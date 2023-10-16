307d1cda0870ef6c14a34b9842214dbf4198f6d0
Thaoho <thao.ho@sotatek.com>
2023-10-16 17:23:58 +0700

    end tuong tac compponent

--- component-interaction/src/app/app.module.ts
+++ component-interaction/src/app/app.module.ts
@@ -2,15 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroChildComponent } from './hero-child.component';
+import { HeroParentComponent } from './hero-parent.component';
+import { NameParentComponent } from './name-parent.component';
+import { NameChildComponent } from './name-child.component';
+import { VersionParentComponent } from './version-parent.component';
+import { VersionChildComponent } from './version-child.component';
+import { VoteTakerComponent } from './votetaker.component';
+import { VoterComponent } from './voter.component';
+import { CountdownLocalVarParentComponent } from './countdown-parent.component';
+import { CountdownTimerComponent } from './countdown-timer.component';
+import { CountdownViewChildParentComponent } from './countdown-parent-view.component';
+import { AstronautComponent } from './astronaut.component';
+import { MissionControlComponent } from './missioncontrol.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroChildComponent,
+    HeroParentComponent,
+    NameParentComponent,
+    NameChildComponent,
+    VersionParentComponent,
+    VersionChildComponent,
+    VoteTakerComponent,
+    VoterComponent,
+    CountdownLocalVarParentComponent,
+    CountdownTimerComponent,
+    CountdownViewChildParentComponent,
+    AstronautComponent,
+    MissionControlComponent,
+
+
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
